e337fb12bb584f46d851da6f9392258ee22e7bc4
mohdbilal123456 <111408918+mohdbilal123456@users.noreply.github.com>
2025-09-04 20:32:43 +0530

    Update adminAuth.js

--- backend/middleware/adminAuth.js
+++ backend/middleware/adminAuth.js
@@ -1,28 +1,27 @@
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
-const adminAuth = async (req,res,next) => {
-    try {
-        let {token} = req.cookies
+const adminAuth = (req, res, next) => {
+  try {
+    let { token } = req.cookies;
 
-    if(!token) {
-        return res.status(400).json({message:"Not Authorized Login Again"})
-    }
-    
-    let verifyToken =  jwt.verify(token,process.env.JWT_SECRET)
-
-    if(!verifyToken){
-         return res.status(400).json({message:"Not Authorized Login Again, Invalid token"})
-    }
-    req.adminEmail = process.env.ADMIN_EMAIL
-
-    next()
-        
-    } catch (error) {
-           console.log("adminAuth error")
-    return res.status(500).json({message:`adminAuth error ${error}`})
+    if (!token) {
+      return res.status(400).json({ message: "Not Authorized Login Again" });
     }
 
+    let verifyToken = jwt.verify(token, process.env.JWT_SECRET);
 
-}
+    if (!verifyToken) {
+      return res.status(400).json({ message: "Not Authorized Login Again, Invalid token" });
+    }
 
-export default adminAuth
+    // Tumhara admin email constant
+    req.adminEmail = process.env.ADMIN_EMAIL;
+
+    next();
+  } catch (error) {
+    console.log("adminAuth error", error);
+    return res.status(500).json({ message: `adminAuth error ${error}` });
+  }
+};
+
+export default adminAuth;
